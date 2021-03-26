edf561682872ecb598bf86ded0410e99f054f683
AlexUTC18 <81427409+AlexUTC18@users.noreply.github.com>
2021-03-26 10:29:45 -0500

    Tercer commit

--- programa.py
+++ programa.py
@@ -14,22 +14,23 @@ def Calculadora():
     Menu()
     opc = int(input("Selecione Opcion\n"))
     while (opc >0 and opc <5):
-        x = int(input("Ingrese Numero\n"))
+        x = int(input("Ingrese un Numero\n"))
         y = int(input("Ingrese Otro Numero\n"))
         if (opc==1):
-            print "resultado de la Suma es:", x+y
+            print "Resultado de la Suma es:", x+y
             opc = int(input("Selecione Opcion\n"))
         elif(opc==2):
-            print "resultado de la Resta es:",x-y
+            print "Resultado de la Resta es:",x-y
             opc = int(input("Selecione Opcion\n"))
         elif(opc==3):
-            print "La Multiplicacion es:",x*y
+            print "Resultado de La Multiplicacion es:",x*y
             opc = int(input("Selecione Opcion\n"))
         elif(opc==4):
             try:
-              print "La Division es:", x/y
+              print "Resultado de La Division es:", x/y
               opc = int(input("Selecione Opcion\n"))
             except ZeroDivisionError:
               print "No se Permite la Division Entre 0"
               opc = int(input("Selecione Opcion\n"))
+               print "Gracias por usar este programa"
 Calculadora()
